ac8d9b47891c9877b9265dac224be1bdcd72674b
keremy <keremyvergaray@gmail.com>
2019-01-11 15:12:35 -0500

    cambio en el index

--- index.php
+++ index.php
@@ -3,10 +3,6 @@ require_once "system/general/config_general.php";
 require "config/constant/config_system.php";
 require "config/constant/config_app.php";
 require "autoload.php";
-
-echo 'Hola Mundo';
-exit();
-
 use \system\route\Route as Route;
 $route=new Route();
 $route->url();
